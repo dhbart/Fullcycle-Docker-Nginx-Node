ddd9e2a057a47476fec3035624defaa63917869a
dhbart <dhbart88@gmail.com>
2022-04-15 23:10:07 -0300

    Update index.js

--- node/index.js
+++ node/index.js
@@ -1,47 +1,55 @@
 const express = require('express')
 const app = express()
-const port = process.env.APP_PORT || 3000
+const port = 3000
+
 const config = {
-        host: 'db',
-        user: 'root',
-        password: 'root',
-        database: 'nodedb'
-};
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
 
-const mysql = require('mysql');
-const Connection = require('mysql/lib/Connection');
-const connection = mysql.createConnection(config)
+const mysql = require('mysql')
+let connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO pessoa(nome) VALUES ('Daniel 4')`;
-connection.query(sql);
-connection.end();
+const createTableQuery = 'CREATE TABLE IF NOT EXISTS pessoa (nome varchar(80) NOT NULL)'
 
-var nomeRetornado = "";
-app.get('/', (req,res) => { 
+connection.query(createTableQuery)
+connection.end()
+
+const names = [
+    'Daniel',
+    'Nome 1',
+    'Nome 2',
+]
+
+
+app.get('/', (req, res) => {
+
+    connection = mysql.createConnection(config)
     
-    var retorno = "<h1>Full Cycle Rocks!</h1>";
-    
-  connection2 = mysql.createConnection(config);
-  connection2.connect(function(err) {
-    if (err) throw err;
-    // if connection is successful
-    connection2.query("SELECT nome FROM pessoa", function (err, result, fields) {
-    // if any error while executing above query, throw error
-    if (err) throw err;
-    // if there is no error, you have the result
-    // iterate for all the rows in result
-        Object.keys(result).forEach(function(key) {
-            var row = result[key].nome;
-            retorno += "<br>" + row;
-        });
-        res.send( retorno );
-    });
-    connection2.end()
-});
+    const randomIndex = Math.floor(Math.random() * names.length);
 
-    
-}) 
+    connection.query(`INSERT INTO pessoa(nome) VALUES ("${names[randomIndex]}")`, (err, result) => {
+        if (err) console.log(err)
+    })
+
+    const defaultResponse = '<h1>Full Cycle Rocks</h1>'
+
+    connection.query("SELECT nome FROM pessoa ORDER BY nome ASC", (err, result) => {
+
+        if (err) console.log(err)
+        result = Object.values(JSON.parse(JSON.stringify(result))).map((row) => ` ${row.nome}`)
+
+        const htmlResponse = [defaultResponse, ...result].join('<br />')
+        res.send(htmlResponse)
+
+    })
+
+
+    connection.end()
+})
 
 app.listen(port, () => {
-    console.log("rodando na porta " + port )
+    console.log(`Rodando na porta: ${port}`)
 })
